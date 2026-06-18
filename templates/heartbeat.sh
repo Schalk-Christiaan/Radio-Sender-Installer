33815de5705e091fe1b4509deee0f2bb0b7ebb2d
Schalk Christiaan Jacobs <89503483+Schalk-Christiaan@users.noreply.github.com>
2026-06-18 15:47:38 +0200

    Toets 2

--- templates/heartbeat.sh
+++ templates/heartbeat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source /opt/radio-orania/config/environment.conf
+
 systemctl is-active --quiet radio-orania.service || exit 1
 
-curl -fsS -o /dev/null "__HEARTBEAT_URL__"
+curl -fsS -o /dev/null "$HEARTBEAT_URL"
