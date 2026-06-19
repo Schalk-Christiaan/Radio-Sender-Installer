55a4fccf0668bdb13636defc7d127f5dfe091de2
Schalk Christiaan Jacobs <89503483+Schalk-Christiaan@users.noreply.github.com>
2026-06-19 10:38:45 +0200

    maak system radio-monitoring.service service reg

--- templates/heartbeat.sh
+++ templates/heartbeat.sh
@@ -4,13 +4,7 @@ source /opt/radio-orania/config/environment.conf
 
 systemctl is-active --quiet radio-orania.service || exit 1
 
-PING=$(curl \
-    -o /dev/null \
-    -s \
-    -w "%{time_total}" \
-    "$STREAM_URL")
-
 curl \
     -fsS \
     -o /dev/null \
-    "${HEARTBEAT_URL}${PING}"
+    "${HEARTBEAT_URL}1"
